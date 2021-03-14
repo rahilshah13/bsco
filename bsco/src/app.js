907a3f0890aaeabda92a8d22072bac5dea18649b
rahilshah13 <rahil@vt.edu>
2021-03-14 03:11:53 -0400

    finished top buttons, mobile view form looks better

--- bsco/src/app.js
+++ bsco/src/app.js
@@ -7,9 +7,7 @@ import Modal from './components/modal';
 import EmojiForm from './components/create_form';
 
 // const headerStyle = isDesktopOrLaptop ? {}: ;
-// const isComputer = useMediaQuery({
-//   query: '(min-device-width: 1224px)'
-// });
+
 const isComputer=true;
 const headerStyle = isComputer
 ? {fontSize: "2vw", padding: "1%"} 
@@ -20,33 +18,54 @@ class App extends Component{
 
 constructor() {
   super();
-  this.state = {show: false};
+  this.state = {show: false, mode: 'connect', matches: window.matchMedia("(min-width: 1224px)").matches};
+
+  this.onChangeMode = this.onChangeMode.bind(this);
   this.showModal = this.showModal.bind(this);
   this.hideModal = this.hideModal.bind(this);
 }
 
 showModal = () => {
   this.setState({ show: true });
+  this.setState({mode: "connect"});
+  console.log(this.state.mode);
 };
 
 hideModal = () => {
   this.setState({ show: false });
 };
 
+onChangeMode = (newMode) => {
+  this.setState({mode: newMode});
+  console.log(this.state.mode);
+};
+
+// componentDidMount() {
+//   // const isComputer = useMediaQuery({
+//   //   query: '(min-device-width: 1224px)'
+//   // });
+//   const mql = window.matchMedia("(min-width: 1224px)");
+//   mql.addEventListener("change", () => {
+//     this.checkNative();
+//   });
+//   console.log(mql)
+// }
+
 render() {
     return (
       <div style={{"textAlign": "center"}}>
         <header style={headerStyle}>
-          <button class="titleChars">🅱️</button>
-          <button class="titleChars">s</button>
-          <button class="titleChars" onClick={this.showModal}>c</button>
-          <button class="titleO">⊕</button> 
+          <button class="titleChars" onClick={()=>this.onChangeMode("breate")}>{this.state.mode === "breate" ? "🅱️" : "b"}</button>
+          <button class="titleChars" onClick={()=>this.onChangeMode("search")}>{this.state.mode === "search" ? "🔎" : "s"}</button>
+          <button class="titleChars" onClick={this.showModal}>{this.state.mode === "connect" ? "🔗" : "c"}</button>
+          <button class="titleChars" onClick={()=>this.onChangeMode("open")}>{this.state.mode === "open" ? "📖": "o"}</button> 
         </header>
-          <Modal show={this.state.show} handleClose={this.hideModal}>
+          <Content isComputer={window.innerWidth > 1000} route={true}/>
+
+          <Modal show={this.state.show} handleClose={this.hideModal} isComputer={window.innerWidth > 1000}>
             <p style={{marginTop:""}}>connect a point</p>
-            <EmojiForm />
+            <EmojiForm isComputer={window.innerWidth > 1000}/>
           </Modal>
-          <Content isComputer={isComputer} />
       </div>
     );
   }
--- bsco/src/components/modal.js
+++ bsco/src/components/modal.js
@@ -1,14 +1,15 @@
 import '../styles/modal.css';
 
-const xButtonStyle = {position: "absolute", right:"0", top:"0", border: "none", outline:"none", backgroundColor: "transparent", fontSize: "1.5vw", padding: ".5vw"};
 
-
-const Modal = ({ handleClose, show, children }) => {
+const Modal = ({ handleClose, show, children, isComputer }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
+  const xButtonStyle = isComputer
+  ? {position: "absolute", right:"0", top:"0", border: "none", outline:"none", backgroundColor: "transparent", fontSize: "1.5vw", padding: ".5vw"}
+  : {position: "absolute", right:"0", top:"0", border: "none", outline:"none", backgroundColor: "transparent", fontSize: "4.5vw", padding: "1.5vw"};
 
   return (
     <div className={showHideClassName}>
-      <section className="modal-main" style={{width: "30%"}}>
+      <section className="modal-main" style={isComputer ? {width: "30%"} : {width: "80%"}}>
         {children}
         <button type="button" onClick={handleClose} style={xButtonStyle}>
             ❌
